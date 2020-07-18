d8675d29fddd02be43757cf9d0e7a0846a41f7a0
h0lms <24info1mail@gmail.com>
2020-07-18 07:01:56 +0300

    vowels JS

---
+++ vowels.js
@@ -0,0 +1,28 @@
+const resultVowels = $(".resut-vowels");
+const btn = $(".form-vowels__btn");
+const input = $(".form-vowels__input");
+const vowels = ['а', 'е', 'у', 'ы', 'о', 'я','ё', 'и', 'э', 'ю'];
+const test = "Привет, как дела?"
+
+btn.on("click", (e) => {
+  e.preventDefault();
+
+  const textInput = input.val().toLowerCase();
+  
+  resultVowels.text(getVowels(textInput));
+});
+
+
+const getVowels = array => {
+  let vovelsStr = "";
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i].toLowerCase();
+    
+    if (vowels.includes(element)) {
+      vovelsStr = vovelsStr + element;
+    }
+  }
+  
+  return vovelsStr;
+}
